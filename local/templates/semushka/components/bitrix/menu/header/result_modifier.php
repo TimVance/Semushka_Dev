6cc1c3fbb2dea99687ee31ef9434e67e54207a47
TimVance <2DngxvshA6GUGcQ>
2020-10-23 16:26:45 +0300

    Меню шапка

---
+++ local/templates/semushka/components/bitrix/menu/header/result_modifier.php
@@ -0,0 +1,56 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+
+
+<?php
+
+/**
+ * @global $arResult
+ */
+
+//echo '<pre>'; print_r($arResult);
+
+$newResult = [];
+$parent = '';
+foreach ($arResult as $i => $item) {
+    if ($item["DEPTH_LEVEL"] == 1) {
+        if (!empty($item["IS_PARENT"])) {
+            $newResult[$i] = $item;
+            $parent1        = $i;
+        } else {
+            $newResult[$i] = $item;
+        }
+    }
+    elseif ($item["DEPTH_LEVEL"] == 2) {
+        if (!empty($item["IS_PARENT"])) {
+            $newResult[$parent1]["CHILDS"][$i] = $item;
+            $parent2        = $i;
+        } else $newResult[$parent1]["CHILDS"][$i]["CHILDS"] = $item;
+
+        $code = str_replace(["catalog", "/"], '', $item["LINK"]);
+        if (!empty($code)) {
+            $seciton_res = CIBlockSection::GetList([], ["CODE" => $code], false, ["ID", "PICTURE"]);
+            while ($section = $seciton_res->GetNext()) {
+                if (!empty($section["PICTURE"])) {
+                    $newResult[$parent1]["CHILDS"][$i]["IMAGE"] = CFile::GetPath($section["PICTURE"]);
+                }
+            }
+        }
+        //$newResult[$i]["IMAGE"] =
+    }
+    elseif ($item["DEPTH_LEVEL"] == 3) {
+        if (!empty($item["IS_PARENT"])) {
+            $newResult[$parent1]["CHILDS"][$parent2]["CHILDS"][$i] = $item;
+            $parent3        = $i;
+        } else $newResult[$parent1]["CHILDS"][$parent2]["CHILDS"][$i] = $item;
+    }
+    elseif ($item["DEPTH_LEVEL"] == 4) {
+        if (!empty($item["IS_PARENT"])) {
+            $newResult[$parent1]["CHILDS"][$parent2]["CHILDS"][$parent3]["CHILDS"][$i] = $item;
+            $parent4        = $i;
+        } else $newResult[$parent1]["CHILDS"][$parent2]["CHILDS"][$parent3]["CHILDS"][$i] = $item;
+    }
+}
+
+$arResult = $newResult;
+
+echo '<pre style="display: none">'; print_r($arResult); echo '</pre>';
--- local/templates/semushka/components/bitrix/menu/header/template.php
+++ local/templates/semushka/components/bitrix/menu/header/template.php
@@ -1,31 +1,70 @@
-<?if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
 
-<?if (!empty($arResult)):?>
-    <ul id="top-nav__list">
-
-    <?
-    $previousLevel = 0;
-foreach($arResult as $arItem):?>
-
-    <?if ($previousLevel && $arItem["DEPTH_LEVEL"] < $previousLevel):?>
-        <?=str_repeat("</ul></li>", ($previousLevel - $arItem["DEPTH_LEVEL"]));?>
-    <?endif?>
-
-    <? if ($arItem["DEPTH_LEVEL"] == 1): ?>
-        <li class="top-nav__item">
-            <a href="<?=$arItem["LINK"]?>" class="top-nav__link">
-                <span class="top-nav__parent"><?=$arItem["TEXT"]?></span>
-            </a>
-        </li>
-    <? endif; ?>
-
-    <?$previousLevel = $arItem["DEPTH_LEVEL"];?>
-
-<?endforeach?>
-
-    <?if ($previousLevel > 1)://close last item tags?>
-        <?=str_repeat("</ul></li>", ($previousLevel-1) );?>
-    <?endif?>
-
+<? if (!empty($arResult)): ?>
+    <ul class="top-nav__list">
+        <? foreach ($arResult as $item1): ?>
+            <? if ($item1["LINK"] == '/catalog/'): ?>
+                <li class="top-nav__item">
+                    <a href="<?=$item1["LINK"]?>" class="top-nav__link">
+                        <span uk-icon="menu"></span>
+                        <span class="top-nav__parent"><?=$item1["TEXT"]?></span>
+                    </a>
+                    <div class="top-nav__childs">
+                        <div class="uk-container">
+                            <div class="main-nav">
+                                <ul class="main-nav__list main-nav__list--lvl1">
+                                    <? foreach ($item1["CHILDS"] as $item2): ?>
+                                        <li class="main-nav__item main-nav__item--lvl1">
+                                            <a href="<?=$item2["LINK"]?>" class="main-nav__link"><?=$item2["TEXT"]?></a>
+                                            <? if (!empty($item2["CHILDS"])): ?>
+                                                <div class="main-nav__childs">
+                                                    <div class="uk-container">
+                                                        <div class="main-nav__list-container main-nav__list-container--lvl2">
+                                                            <ul class="main-nav__list main-nav__list--lvl2">
+                                                                <? foreach ($item2["CHILDS"] as $item3): ?>
+                                                                    <li class="main-nav__item main-nav__item--lvl2">
+                                                                        <a href="<?=$item3["LINK"]?>" class="main-nav__link"><?=$item3["TEXT"]?></a>
+                                                                        <? if (!empty($item3["CHILDS"])): ?>
+                                                                            <div class="main-nav__childs">
+                                                                                <div class="main-nav__list-container main-nav__list-container--lvl3">
+                                                                                    <ul class="main-nav__list main-nav__list--lvl3">
+                                                                                        <? foreach ($item3["CHILDS"] as $item4): ?>
+                                                                                            <li class="main-nav__item main-nav__item--lvl3">
+                                                                                                <a href="<?=$item4["LINK"]?>" class="main-nav__link"><?=$item4["TEXT"]?></a>
+                                                                                            </li>
+                                                                                        <? endforeach; ?>
+                                                                                    </ul>
+                                                                                </div>
+                                                                            </div>
+                                                                        <? endif; ?>
+                                                                    </li>
+                                                                <? endforeach; ?>
+                                                            </ul>
+                                                        </div>
+                                                        <div class="main-nav__image">
+                                                            <div class="main-nav__img">
+                                                                <? if (!empty($item2["IMAGE"])): ?>
+                                                                    <img src="<?=$item2["IMAGE"]?>" alt="<?=$item2["TEXT"]?>">
+                                                                <? endif; ?>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            <? endif; ?>
+                                        </li>
+                                    <? endforeach; ?>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+            <? else: ?>
+                <li class="top-nav__item">
+                    <a href="<?=$item1["LINK"]?>" class="top-nav__link">
+                        <span><?=$item1["TEXT"]?></span>
+                    </a>
+                </li>
+            <? endif; ?>
+        <? endforeach; ?>
     </ul>
-<?endif?>
+<? endif ?>
